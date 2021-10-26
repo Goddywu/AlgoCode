9219951a4290a0a4f2abf6df4c0071819b713218
Goddy <goddy@mykg.ai>
2021-10-26 23:33:38 +0800

    update on 2021-10-26

---
+++ leetcode/c347_前k个高频元素/Main.java
@@ -0,0 +1,26 @@
+package c347_前k个高频元素;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int n : nums) {
+            int count = map.getOrDefault(n, 0) + 1;
+            map.put(n, count);
+            max = Math.max(max, count);
+        }
+
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(
+                (n1, n2) -> n2.getValue() - n1.getValue());
+        queue.addAll(map.entrySet());
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll().getKey();
+        }
+        return ans;
+    }
+}
