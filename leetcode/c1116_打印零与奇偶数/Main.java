358f5f5bbeb9a6ca72b3c5cfa13d4cde6c0e4ea8
Goddy <goddy@mykg.ai>
2021-10-28 22:28:53 +0800

    init

---
+++ leetcode/c1116_打印零与奇偶数/Main.java
@@ -0,0 +1,8 @@
+package c1116_打印零与奇偶数;
+
+// https://leetcode-cn.com/problems/print-zero-even-odd/
+public class Main {
+    static class ZeroEvenOdd {
+        // fixme
+    }
+}
---
+++ leetcode/c433_最小基因变化/Main.java
@@ -0,0 +1,10 @@
+package c433_最小基因变化;
+
+public class Main {
+
+    // https://leetcode-cn.com/problems/minimum-genetic-mutation/
+    public int minMutation(String start, String end, String[] bank) {
+        // FIXME:
+        return 0;
+    }
+}
