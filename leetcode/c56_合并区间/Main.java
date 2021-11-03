f51d5eea2fabb7253b77dc11749b29358c221287
Goddy <goddy@mykg.ai>
2021-11-04 01:07:31 +0800

    update on 2021-11-04

---
+++ leetcode/c56_合并区间/Main.java
@@ -0,0 +1,30 @@
+package c56_合并区间;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+  public int[][] merge(int[][] intervals) {
+    if (intervals.length == 0) return new int[0][2];
+
+    Arrays.sort(intervals, (i1, i2) -> i1[0] - i2[0]);
+
+    List<int[]> ans = new ArrayList<>();
+    int[] curInts = intervals[0];
+    int po = 1;
+    while (po < intervals.length) {
+      int[] nums = intervals[po];
+      if (curInts[1] < nums[0]) {
+        ans.add(curInts);
+        curInts = nums;
+      } else {
+        curInts = new int[]{curInts[0], Math.max(nums[1], curInts[1])};
+      }
+      po++;
+    }
+    ans.add(curInts);
+    return ans.toArray(new int[ans.size()][]);
+  }
+}
